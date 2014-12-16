goldfirere/ghc

commit 0cc47eb90805f3e166ac4d3991e66d3346ca05e7

Rewrite `Coercible` solver
Summary:
This is a rewrite of the algorithm to solve for Coercible "instances".

A preliminary form of these ideas is at
https://ghc.haskell.org/trac/ghc/wiki/Design/NewCoercibleSolver

The basic idea here is that the `EqPred` constructor of `PredTree`
now is parameterised by a new type `EqRel` (where
`data EqRel = NomEq | ReprEq`). Thus, every equality constraint can
now talk about nominal equality (the usual case) or representational
equality (the `Coercible` case).

This is a change from the previous
behavior where `Coercible` was just considered a regular class with
a special case in `matchClassInst`.

Because of this change, representational equalities are now
canonicalized just like nominal ones, allowing more equalities
to be solved -- in particular, the case at the top of #9117.

A knock-on effect is that the flattener must be aware of the
choice of equality relation, because the inert set now stores
both representational inert equalities alongside the nominal
inert equalities. Of course, we can use representational equalities
to rewrite only within another representational equality --
thus the parameterization of the flattener.

A nice side effect of this change is that I've introduced a new
type `CtFlavour`, which tracks G vs. W vs. D, removing some ugliness
in the flattener.

This commit includes some refactoring as discussed on D546.
It also removes the ability of Deriveds to rewrite Deriveds.

This fixes bugs #9117 and #8984.

Reviewers: simonpj, austin, nomeata

Subscribers: carter, thomie

Differential Revision: https://phabricator.haskell.org/D546

GHC Trac Issues: #9117, #8984

compiler/utils/MonadUtils.hs
@@ -11,7 +11,7 @@ module MonadUtils
 
         , liftIO1, liftIO2, liftIO3, liftIO4
 
-        , zipWith3M
+        , zipWith3M, zipWith3M_, zipWithAndUnzipM
         , mapAndUnzipM, mapAndUnzip3M, mapAndUnzip4M
         , mapAccumLM
         , mapSndM
@@ -70,6 +70,18 @@ zipWith3M f (x:xs) (y:ys) (z:zs)
        ; rs <- zipWith3M f xs ys zs
        ; return $ r:rs
        }
+
+zipWith3M_ :: Monad m => (a -> b -> c -> m d) -> [a] -> [b] -> [c] -> m ()
+zipWith3M_ f as bs cs = do { _ <- zipWith3M f as bs cs
+                           ; return () }
+
+zipWithAndUnzipM :: Monad m
+                 => (a -> b -> m (c, d)) -> [a] -> [b] -> m ([c], [d])
+zipWithAndUnzipM f (x:xs) (y:ys)
+  = do { (c, d) <- f x y
+       ; (cs, ds) <- zipWithAndUnzipM f xs ys
+       ; return (c:cs, d:ds) }
+zipWithAndUnzipM _ _ _ = return ([], [])
 
 -- | mapAndUnzipM for triples
 mapAndUnzip3M :: Monad m => (a -> m (b,c,d)) -> [a] -> m ([b],[c],[d])

testsuite/tests/deriving/should_fail/all.T
@@ -53,3 +53,4 @@ test('T9071', normal, multimod_compile_fail, ['T9071',''])
 test('T9071_2', normal, compile_fail, [''])
 test('T9687', normal, compile_fail, [''])
 
+test('T8984', normal, compile_fail, [''])

testsuite/tests/typecheck/should_compile/all.T
@@ -423,8 +423,9 @@ test('MutRec', normal, compile, [''])
 test('T8856', normal, compile, [''])
 test('T9569a', normal, compile, [''])
 test('T9117', normal, compile, [''])
-test('T9117_2', expect_broken('9117'), compile, [''])
-test('T9708', normal, compile, [''])
+test('T9117_2', normal, compile, [''])
+test('T9117_3', normal, compile, [''])
+test('T9708', expect_broken(9708), compile, [''])
 test('T9404', normal, compile, [''])
 test('T9404b', normal, compile, [''])
 test('T7220', normal, compile, [''])

compiler/basicTypes/DataCon.hs
@@ -971,9 +971,9 @@ dataConCannotMatch tys con
 
     -- TODO: could gather equalities from superclasses too
     predEqs pred = case classifyPredType pred of
-                     EqPred ty1 ty2 -> [(ty1, ty2)]
-                     TuplePred ts   -> concatMap predEqs ts
-                     _              -> []
+                     EqPred NomEq ty1 ty2 -> [(ty1, ty2)]
+                     TuplePred ts         -> concatMap predEqs ts
+                     _                    -> []
 
 {-
 ************************************************************************

compiler/deSugar/DsBinds.hs
@@ -951,9 +951,7 @@ ds_tc_coercion subst tc_co
   where
     go (TcRefl r ty)            = Refl r (Coercion.substTy subst ty)
     go (TcTyConAppCo r tc cos)  = mkTyConAppCo r tc (map go cos)
-    go (TcAppCo co1 co2)        = let leftCo    = go co1
-                                      rightRole = nextRole leftCo in
-                                  mkAppCoFlexible leftCo rightRole (go co2)
+    go (TcAppCo co1 co2)        = mkAppCo (go co1) (go co2)
     go (TcForAllCo tv co)       = mkForAllCo tv' (ds_tc_coercion subst' co)
                               where
                                 (subst', tv') = Coercion.substTyVarBndr subst tv
@@ -969,6 +967,7 @@ ds_tc_coercion subst tc_co
     go (TcCastCo co1 co2)       = mkCoCast (go co1) (go co2)
     go (TcCoVarCo v)            = ds_ev_id subst v
     go (TcAxiomRuleCo co ts cs) = AxiomRuleCo co (map (Coercion.substTy subst) ts) (map go cs)
+    go (TcCoercion co)          = co
 
     ds_co_binds :: TcEvBinds -> CvSubst
     ds_co_binds (EvBinds bs)      = foldl ds_scc subst (sccEvBinds bs)

compiler/typecheck/FamInst.hs
@@ -6,18 +6,17 @@ module FamInst (
         FamInstEnvs, tcGetFamInstEnvs,
         checkFamInstConsistency, tcExtendLocalFamInstEnv,
         tcLookupFamInst,
-        tcLookupDataFamInst, tcInstNewTyConTF_maybe, tcInstNewTyCon_maybe,
+        tcLookupDataFamInst, tcLookupDataFamInst_maybe,
+        tcInstNewTyCon_maybe, tcTopNormaliseNewTypeTF_maybe,
         newFamInst
     ) where
 
 import HscTypes
 import FamInstEnv
 import InstEnv( roughMatchTcs )
-import Coercion( pprCoAxBranchHdr )
+import Coercion    hiding ( substTy )
 import TcEvidence
 import LoadIface
-import Type( applyTysX )
-import TypeRep
 import TcRnMonad
 import TyCon
 import CoAxiom
@@ -27,6 +26,8 @@ import Outputable
 import UniqFM
 import FastString
 import Util
+import RdrName
+import DataCon ( dataConName )
 import Maybes
 import TcMType
 import TcType
@@ -34,6 +35,7 @@ import Name
 import Control.Monad
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Control.Arrow ( first, second )
 
 #include "HsVersions.h"
 
@@ -216,44 +218,79 @@ tcLookupFamInst fam_envs tycon tys
 -- Checks for a newtype, and for being saturated
 -- Just like Coercion.instNewTyCon_maybe, but returns a TcCoercion
 tcInstNewTyCon_maybe :: TyCon -> [TcType] -> Maybe (TcType, TcCoercion)
-tcInstNewTyCon_maybe tc tys
-  | Just (tvs, ty, co_tc) <- unwrapNewTyConEtad_maybe tc  -- Check for newtype
-  , tvs `leLength` tys                                    -- Check saturated enough
-  = Just (applyTysX tvs ty tys, mkTcUnbranchedAxInstCo Representational co_tc tys)
-  | otherwise
-  = Nothing
+tcInstNewTyCon_maybe tc tys = fmap (second TcCoercion) $
+                              instNewTyCon_maybe tc tys
 
+-- | Like 'tcLookupDataFamInst_maybe', but returns the arguments back if
+-- there is no data family to unwrap.
 tcLookupDataFamInst :: FamInstEnvs -> TyCon -> [TcType]
                     -> (TyCon, [TcType], TcCoercion)
+tcLookupDataFamInst fam_inst_envs tc tc_args
+  | Just (rep_tc, rep_args, co)
+      <- tcLookupDataFamInst_maybe fam_inst_envs tc tc_args
+  = (rep_tc, rep_args, TcCoercion co)
+  | otherwise
+  = (tc, tc_args, mkTcRepReflCo (mkTyConApp tc tc_args))
+
+tcLookupDataFamInst_maybe :: FamInstEnvs -> TyCon -> [TcType]
+                          -> Maybe (TyCon, [TcType], Coercion)
 -- ^ Converts a data family type (eg F [a]) to its representation type (eg FList a)
 -- and returns a coercion between the two: co :: F [a] ~R FList a
--- If there is no instance, or it's not a data family, just return
--- Refl coercion and the original inputs
-tcLookupDataFamInst fam_inst_envs tc tc_args
+tcLookupDataFamInst_maybe fam_inst_envs tc tc_args
   | isDataFamilyTyCon tc
   , match : _ <- lookupFamInstEnv fam_inst_envs tc tc_args
   , FamInstMatch { fim_instance = rep_fam
                  , fim_tys      = rep_args } <- match
   , let co_tc  = famInstAxiom rep_fam
         rep_tc = dataFamInstRepTyCon rep_fam
-        co     = mkTcUnbranchedAxInstCo Representational co_tc rep_args
-  = (rep_tc, rep_args, co)
+        co     = mkUnbranchedAxInstCo Representational co_tc rep_args
+  = Just (rep_tc, rep_args, co)
 
-  | otherwise
-  = (tc, tc_args, mkTcNomReflCo (mkTyConApp tc tc_args))
-
-tcInstNewTyConTF_maybe :: FamInstEnvs -> TcType -> Maybe (TyCon, TcType, TcCoercion)
--- ^ If (instNewTyConTF_maybe envs ty) returns Just (ty', co)
---   then co :: ty ~R ty'
---        ty is (D tys) is a newtype (possibly after looking through the type family D)
---        ty' is the RHS type of the of (D tys) newtype
-tcInstNewTyConTF_maybe fam_envs ty
-  | Just (tc, tc_args) <- tcSplitTyConApp_maybe ty
-  , let (rep_tc, rep_tc_args, fam_co) = tcLookupDataFamInst fam_envs tc tc_args
-  , Just (inner_ty, nt_co) <- tcInstNewTyCon_maybe rep_tc rep_tc_args
-  = Just (rep_tc, inner_ty, fam_co `mkTcTransCo` nt_co)
   | otherwise
   = Nothing
+
+-- | Get rid of top-level newtypes, potentially looking through newtype
+-- instances. Only unwraps newtypes that are in scope. This is used
+-- for solving for `Coercible` in the solver. This version is careful
+-- not to unwrap data/newtype instances if it can't continue unwrapping.
+-- Such care is necessary for proper error messages.
+--
+-- Does not look through type families. Does not normalise arguments to a
+-- tycon.
+--
+-- Always produces a representational coercion.
+tcTopNormaliseNewTypeTF_maybe :: FamInstEnvs
+                              -> GlobalRdrEnv
+                              -> Type
+                              -> Maybe (TcCoercion, Type)
+tcTopNormaliseNewTypeTF_maybe faminsts rdr_env ty
+-- cf. FamInstEnv.topNormaliseType_maybe and Coercion.topNormaliseNewType_maybe
+  = fmap (first TcCoercion) $ topNormaliseTypeX_maybe stepper ty
+  where
+    stepper
+      = unwrap_newtype
+        `composeSteppers`
+        \ rec_nts tc tys ->
+        case tcLookupDataFamInst_maybe faminsts tc tys of
+          Just (tc', tys', co) ->
+            modifyStepResultCo (co `mkTransCo`)
+                               (unwrap_newtype rec_nts tc' tys')
+          Nothing -> NS_Done
+
+    unwrap_newtype rec_nts tc tys
+      | data_cons_in_scope tc
+      = unwrapNewTypeStepper rec_nts tc tys
+
+      | otherwise
+      = NS_Done
+
+    data_cons_in_scope :: TyCon -> Bool
+    data_cons_in_scope tc
+      = isWiredInName (tyConName tc) ||
+        (not (isAbstractTyCon tc) && all in_scope data_con_names)
+      where
+        data_con_names = map dataConName (tyConDataCons tc)
+        in_scope dc    = not $ null $ lookupGRE_Name rdr_env dc
 
 {-
 ************************************************************************

compiler/typecheck/FunDeps.hs
@@ -480,9 +480,9 @@ oclose preds fixed_tvs
                do let (cls_tvs, cls_fds) = classTvsFds cls
                   fd <- cls_fds
                   return (instFD fd cls_tvs tys)
-            EqPred t1 t2      -> [([t1],[t2]), ([t2],[t1])]
-            TuplePred ts      -> concatMap determined ts
-            _                 -> []
+            EqPred NomEq t1 t2 -> [([t1],[t2]), ([t2],[t1])]
+            TuplePred ts       -> concatMap determined ts
+            _                  -> []
 
 {-
 ************************************************************************

commit f2617589b7c3b984e7afd002038a89de61e9f154

Performance enhancements in TcFlatten.
This commit fixes some performance regressions introduced by 0cc47eb,
adding more `Coercible` magic to the solver. See Note
[flatten_many performance] in TcFlatten for more info.

The improvements do not quite restore the old numbers. Given that
the solver is really more involved now, I am accepting this regression.

The way forward (I believe) would be to have *two* flatteners: one
that deals only with nominal equalities and thus never checks roles,
and the more general one. A nice design of keeping this performant
without duplicating code eludes me, but someone else is welcome
to take a stab.

compiler/typecheck/TcFlatten.hs
@@ -28,10 +28,11 @@ import TcSMonad as TcS
 import DynFlags( DynFlags )
 
 import Util
-import MonadUtils   ( zipWithAndUnzipM )
 import Bag
 import FastString
 import Control.Monad( when, liftM )
+import MonadUtils ( zipWithAndUnzipM )
+import GHC.Exts ( inline )
 
 {-
 Note [The flattening story]
@@ -643,6 +644,37 @@ soon throw out the phantoms when decomposing a TyConApp. (Or, the
 canonicaliser will emit an insoluble, in which case the unflattened version
 yields a better error message anyway.)
 
+Note [flatten_many performance]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+In programs with lots of type-level evaluation, flatten_many becomes
+part of a tight loop. For example, see test perf/compiler/T9872a, which
+calls flatten_many a whopping 7,106,808 times. It is thus important
+that flatten_many be efficient.
+
+Performance testing showed that the current implementation is indeed
+efficient. It's critically important that zipWithAndUnzipM be
+specialized to TcS, and it's also quite helpful to actually `inline`
+it. On test T9872a, here are the allocation stats (Dec 16, 2014):
+
+ * Unspecialized, uninlined:     8,472,613,440 bytes allocated in the heap
+ * Specialized, uninlined:       6,639,253,488 bytes allocated in the heap
+ * Specialized, inlined:         6,281,539,792 bytes allocated in the heap
+
+To improve performance even further, flatten_many_nom is split off
+from flatten_many, as nominal equality is the common case. This would
+be natural to write using mapAndUnzipM, but even inlined, that function
+is not as performant as a hand-written loop.
+
+ * mapAndUnzipM, inlined:        7,463,047,432 bytes allocated in the heap
+ * hand-written recursion:       5,848,602,848 bytes allocated in the heap
+
+If you make any change here, pay close attention to the T9872{a,b,c} tests
+and T5321Fun.
+
+If we need to make this yet more performant, a possible way forward is to
+duplicate the flattener code for the nominal case, and make that case
+faster. This doesn't seem quite worth it, yet.
+
 -}
 
 ------------------
@@ -676,12 +708,23 @@ flatten_many :: FlattenEnv -> [Role] -> [Type] -> TcS ([Xi], [TcCoercion])
 --     we merely want (a) Given/Solved/Derived/Wanted info
 --                    (b) the GivenLoc/WantedLoc for when we create new evidence
 flatten_many fmode roles tys
-  = zipWithAndUnzipM go roles tys
+-- See Note [flatten_many performance]
+  = inline zipWithAndUnzipM go roles tys
   where
-    go Nominal          ty = flatten_one (fmode { fe_eq_rel = NomEq })  ty
-    go Representational ty = flatten_one (fmode { fe_eq_rel = ReprEq }) ty
+    go Nominal          ty = flatten_one (setFEEqRel fmode NomEq)  ty
+    go Representational ty = flatten_one (setFEEqRel fmode ReprEq) ty
     go Phantom          ty = -- See Note [Phantoms in the flattener]
                              return (ty, mkTcPhantomCo ty ty)
+
+-- | Like 'flatten_many', but assumes that every role is nominal.
+flatten_many_nom :: FlattenEnv -> [Type] -> TcS ([Xi], [TcCoercion])
+flatten_many_nom _     [] = return ([], [])
+-- See Note [flatten_many performance]
+flatten_many_nom fmode (ty:tys)
+  = ASSERT( fe_eq_rel fmode == NomEq )
+    do { (xi, co) <- flatten_one fmode ty
+       ; (xis, cos) <- flatten_many_nom fmode tys
+       ; return (xi:xis, co:cos) }
 
 ------------------
 flatten_one :: FlattenEnv -> TcType -> TcS (Xi, TcCoercion)
@@ -707,7 +750,7 @@ flatten_one fmode (AppTy ty1 ty2)
              return (mkAppTy xi1 ty2, co1 `mkTcAppCo` mkTcNomReflCo ty2) }
   where
     flatten_rhs xi1 co1 eq_rel2
-      = do { (xi2,co2) <- flatten_one (fmode { fe_eq_rel = eq_rel2 }) ty2
+      = do { (xi2,co2) <- flatten_one (setFEEqRel fmode eq_rel2) ty2
            ; traceTcS "flatten/appty"
                       (ppr ty1 $$ ppr ty2 $$ ppr xi1 $$
                        ppr co1 $$ ppr xi2 $$ ppr co2)
@@ -757,14 +800,16 @@ flatten_one fmode ty@(ForAllTy {})
 -- We allow for-alls when, but only when, no type function
 -- applications inside the forall involve the bound type variables.
   = do { let (tvs, rho) = splitForAllTys ty
-       ; (rho', co) <- flatten_one (fmode { fe_mode = FM_SubstOnly }) rho
+       ; (rho', co) <- flatten_one (setFEMode fmode FM_SubstOnly) rho
                          -- Substitute only under a forall
                          -- See Note [Flattening under a forall]
        ; return (mkForAllTys tvs rho', foldr mkTcForAllCo co tvs) }
 
 flattenTyConApp :: FlattenEnv -> TyCon -> [TcType] -> TcS (Xi, TcCoercion)
 flattenTyConApp fmode tc tys
-  = do { (xis, cos) <- flatten_many fmode (tyConRolesX role tc) tys
+  = do { (xis, cos) <- case fe_eq_rel fmode of
+                         NomEq  -> flatten_many_nom fmode tys
+                         ReprEq -> flatten_many fmode (tyConRolesX role tc) tys
        ; return (mkTyConApp tc xis, mkTcTyConAppCo role tc cos) }
   where
     role = feRole fmode
@@ -855,8 +900,7 @@ flatten_exact_fam_app fmode tc tys
     roles = tyConRolesX (feRole fmode) tc
 
 flatten_exact_fam_app_fully fmode tc tys
-  = do { let roles = tyConRolesX (feRole fmode) tc
-       ; (xis, cos) <- flatten_many (fmode { fe_mode = FM_FlattenAll }) roles tys
+  = do { (xis, cos) <- flatten_many_nom (setFEEqRel (setFEMode fmode FM_FlattenAll) NomEq) tys
        ; let ret_co = mkTcTyConAppCo (feRole fmode) tc cos
               -- ret_co :: F xis ~ F tys
 
@@ -1222,7 +1266,7 @@ flattenTyVarFinal :: FlattenEnv -> TcTyVar -> TcS TyVar
 flattenTyVarFinal fmode tv
   = -- Done, but make sure the kind is zonked
     do { let kind       = tyVarKind tv
-             kind_fmode = fmode { fe_mode = FM_SubstOnly }
+             kind_fmode = setFEMode fmode FM_SubstOnly
        ; (new_knd, _kind_co) <- flatten_one kind_fmode kind
        ; return (setVarType tv new_knd) }
 
@@ -1506,3 +1550,22 @@ unsolved constraints.  The flat form will be
 
 Flatten using the fun-eqs first.
 -}
+
+-- | Change the 'EqRel' in a 'FlattenEnv'. Avoids allocating a
+-- new 'FlattenEnv' where possible.
+setFEEqRel :: FlattenEnv -> EqRel -> FlattenEnv
+setFEEqRel fmode@(FE { fe_eq_rel = old_eq_rel }) new_eq_rel
+  | old_eq_rel == new_eq_rel = fmode
+  | otherwise                = fmode { fe_eq_rel = new_eq_rel }
+
+-- | Change the 'FlattenMode' in a 'FlattenEnv'. Avoids allocating
+-- a new 'FlattenEnv' where possible.
+setFEMode :: FlattenEnv -> FlattenMode -> FlattenEnv
+setFEMode fmode@(FE { fe_mode = old_mode }) new_mode
+  | old_mode `eq` new_mode = fmode
+  | otherwise            = fmode { fe_mode = new_mode }
+  where
+    FM_FlattenAll   `eq` FM_FlattenAll   = True
+    FM_SubstOnly    `eq` FM_SubstOnly    = True
+    FM_Avoid tv1 b1 `eq` FM_Avoid tv2 b2 = tv1 == tv2 && b1 == b2
+    _               `eq` _               = False

compiler/utils/MonadUtils.hs
@@ -77,6 +77,9 @@ zipWith3M_ f as bs cs = do { _ <- zipWith3M f as bs cs
 
 zipWithAndUnzipM :: Monad m
                  => (a -> b -> m (c, d)) -> [a] -> [b] -> m ([c], [d])
+{-# INLINE zipWithAndUnzipM #-}
+-- See Note [flatten_many performance] in TcFlatten for why this
+-- pragma is essential.
 zipWithAndUnzipM f (x:xs) (y:ys)
   = do { (c, d) <- f x y
        ; (cs, ds) <- zipWithAndUnzipM f xs ys

testsuite/tests/perf/compiler/all.T
@@ -406,7 +406,7 @@ test('T5321Fun',
              #  (increase due to new codegen)
              # 2014-09-03: 299656164     (specialisation and inlining)
              # 10/12/2014: 206406188     #  Improvements in constraint solver
-            (wordsize(64), 408110888, 10)])
+            (wordsize(64), 429921312, 10)])
              # prev:       585521080
              # 29/08/2012: 713385808     #  (increase due to new codegen)
              # 15/05/2013: 628341952     #  (reason for decrease unknown)
@@ -415,6 +415,7 @@ test('T5321Fun',
              # 10/09/2014: 601629032     #  post-AMP-cleanup
              # 06/11/2014: 541287000     #  Simon's flat-skol changes to the constraint solver
              # 10/12/2014: 408110888     #  Improvements in constraint solver
+             # 16/12/2014: 429921312     #  Flattener parameterized over roles
       ],
       compile,[''])
 
@@ -477,7 +478,7 @@ test('T5837',
              # 2014-12-01: 135914136 (Windows laptop, regression see below)
              # 2014-12-08  115905208  Constraint solver perf improvements (esp kick-out)
  
-           (wordsize(64), 234790312, 10)])
+           (wordsize(64), 231155640, 10)])
              # sample: 3926235424 (amd64/Linux, 15/2/2012)
              # 2012-10-02 81879216
              # 2012-09-20 87254264 amd64/Linux
@@ -489,6 +490,8 @@ test('T5837',
              # 2014-11-06 271028976       Linux, Accept big regression;
              #   See Note [An alternative story for the inert substitution] in TcFlatten
              # 2014-12-08 234790312 Constraint solver perf improvements (esp kick-out)
+             # 2014-12-16 231155640 Mac  Flattener parameterized over roles;
+             #                           some optimization
       ],
       compile_fail,['-ftype-function-depth=50'])
 
@@ -556,8 +559,9 @@ test('T9675',
 test('T9872a',
      [ only_ways(['normal']),
        compiler_stats_num_field('bytes allocated',
-          [(wordsize(64), 5521332656, 5)
+          [(wordsize(64), 5848657456, 5)
           # 2014-12-10    5521332656    Initally created
+          # 2014-12-16    5848657456    Flattener parameterized over roles
           ]),
       ],
      compile_fail,
@@ -566,8 +570,9 @@ test('T9872a',
 test('T9872b',
      [ only_ways(['normal']),
        compiler_stats_num_field('bytes allocated',
-          [(wordsize(64), 6483306280, 5)
+          [(wordsize(64), 6892251912, 5)
           # 2014-12-10    6483306280    Initally created
+          # 2014-12-16    6892251912    Flattener parameterized over roles
           ]),
       ],
      compile_fail,
@@ -575,8 +580,9 @@ test('T9872b',
 test('T9872c',
      [ only_ways(['normal']),
        compiler_stats_num_field('bytes allocated',
-          [(wordsize(64), 5495850096, 5)
+          [(wordsize(64), 5842024784, 5)
           # 2014-12-10    5495850096    Initally created
+          # 2014-12-16    5842024784    Flattener parameterized over roles
           ]),
       ],
      compile_fail,